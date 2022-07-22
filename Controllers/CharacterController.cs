99935f4623e98fd952b53f66621a2357131a40bf
Bryant Wooters <Jesusrox45@gmail.com>
2022-07-21 22:46:57 -0400

    Initial commit

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+    private static List<Character> characters = new List<Character>
+    {
+        new Character(),
+        new Character{ Id = 1, Name = "Sam" }
+    };
+
+    [HttpGet]
+    [Route("GetAll")]
+    public ActionResult<List<Character>> Get()
+    {
+        return Ok(characters);
+    }
+    
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<Character> GetSingle(int id)
+    {
+        return Ok(characters.FirstOrDefault(c => c.Id == id));
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace dotnet_rpg.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum RpgClass
+{
+    Knight = 1,
+    Mage,
+    Cleric
+}
